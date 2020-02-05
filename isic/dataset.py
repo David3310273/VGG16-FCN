ebc189dd3a32db0e4d20817d4628dc5a26d1ec81
David <2319712368@qq.com>
2020-02-05 17:00:57 +0800

    Feat: add script for isic dataset

---
+++ isic/dataset.py
@@ -0,0 +1,190 @@
+import os
+import random
+import numpy as np
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+import math
+import torchvision.transforms.functional as TF
+from processing import augment_transform, threshold_by_ostu
+
+
+class IsicDataset(Dataset):
+    def __init__(self, path_dict, need_aug=True, is_test=False):
+        """
+        :param pathDict: 必须包括"train_img", "train_gt"字段, 以及对应的数据集列表
+        """
+        super().__init__()
+
+        self.path_dict = path_dict
+        self.data_aug = need_aug
+        self.model_size = (224, 224)    # limitted by VGG16 backbone
+        self.is_test = is_test
+
+        # 预先计算dataset个数和包含的图片的个数
+        dum_dataset_path = path_dict["train_img"] if not is_test else path_dict["test_img"]
+        self.images_count = len(os.listdir(dum_dataset_path))
+
+        self.__make_dataset()
+
+    def general_transform(self, img):
+        """
+        :param img: PIL image
+        :return:
+        """
+        image = TF.resize(img, self.model_size)
+        return image
+
+    def __make_dataset(self):
+        datadict = self.path_dict
+        assert "train_img" in datadict and "train_gt" in datadict
+        assert "train_pos_gt" in datadict
+        assert "test_img" in datadict and "test_gt" in datadict
+        assert "test_pos_gt" in datadict
+
+        # 按照dataset目录加载图片的绝对路径, 后续可以设置具体需要加载的dataset
+        img_path = datadict["train_img"]
+        images = []
+
+        imgs = os.listdir(img_path)
+        imgs.sort()  # 保证按时间顺序加载帧
+        for img in imgs:
+            img_route = os.path.join(img_path, img)
+            images.append(img_route)
+
+        # 按照dataset目录加载ground truth
+        img_path = datadict["train_gt"]
+
+        ground_truths = []
+
+        imgs = os.listdir(img_path)
+        imgs.sort()  # 保证按时间顺序加载帧
+        for img in imgs:
+            img_route = os.path.join(img_path, img)
+            ground_truths.append(img_route)
+
+        img_path = datadict["train_pos_gt"]
+        pos_gts = []
+
+        if img_path:
+            imgs = os.listdir(img_path)
+            imgs.sort()  # 保证按时间顺序加载帧
+            for img in imgs:
+                img_route = os.path.join(img_path, img)
+                pos_gts.append(img_route)
+
+        # 加载测试数据集
+
+        img_path = datadict["test_img"]
+        test_images = []
+
+        imgs = os.listdir(img_path)
+        imgs.sort()  # 保证按时间顺序加载帧
+        for img in imgs:
+            img_route = os.path.join(img_path, img)
+            test_images.append(img_route)
+
+        # 按照dataset目录加载test ground truth
+        img_path = datadict["test_gt"]
+
+        test_ground_truths = []
+
+        imgs = os.listdir(img_path)
+        imgs.sort()  # 保证按时间顺序加载帧
+        for img in imgs:
+            img_route = os.path.join(img_path, img)
+            test_ground_truths.append(img_route)
+
+        img_path = datadict["test_pos_gt"]
+        test_pos_gts = []
+
+        if img_path:
+            imgs = os.listdir(img_path)
+            imgs.sort()  # 保证按时间顺序加载帧
+            for img in imgs:
+                img_route = os.path.join(img_path, img)
+                test_pos_gts.append(img_route)
+
+        self.train_images = images
+        self.train_ground_truths = ground_truths
+        self.train_pos_gts = pos_gts
+
+        self.test_images = test_images
+        self.test_ground_truths = test_ground_truths
+        self.test_pos_gts = test_pos_gts
+
+
+    def __len__(self):
+        return len(self.train_images) if not self.is_test else len(self.test_images)
+
+
+    def threshold_transform(self, img):
+        """
+        传入0-255的灰度PIL图，给出分割后的PIL图片
+        :param img:
+        :return:
+        """
+        image = np.array(img)
+        threshold = threshold_by_ostu(image)
+        image[image > threshold] = 255
+        image[image <= threshold] = 0
+
+        return Image.fromarray(image)
+
+    def __getitem__(self, index):
+        # pick out指定位置的图片路径，再进行真正的图片加载
+        print("{} => dataset {} and offset {}".format(index, "training" if not self.is_test else "testing", index))
+        image = []
+        ground_truth = []
+        pos_gt = []
+        filenames = []
+
+        # 真正加载图片
+        if not self.is_test:
+            raw_image = self.general_transform(Image.open(self.train_images[index]))    # 裁黑边并进行resize
+            raw_ground_truth = self.general_transform(Image.open(self.train_ground_truths[index]))  # ground truth转为灰度图用于最终loss的计算
+            raw_pos_fake_ground_truth = self.general_transform(Image.open(self.train_pos_gts[index]))
+            filenames = (self.train_images[index]).split(".")[0]
+            raw_pos_fake_ground_truth = self.threshold_transform(raw_pos_fake_ground_truth)
+
+            print("loading image and ground truth from...")
+            print(self.train_images[index])
+            print(self.train_ground_truths[index])
+            print(self.train_pos_gts[index])
+            print(filenames)
+        else:
+            raw_image = self.general_transform(Image.open(self.test_images[index]))  # 裁黑边并进行resize
+            raw_ground_truth = self.general_transform(
+                Image.open(self.test_ground_truths[index]))  # ground truth转为灰度图用于最终loss的计算
+            raw_pos_fake_ground_truth = self.general_transform(Image.open(self.test_pos_gts[index]))
+            filenames = (self.test_images[index]).split(".")[0]
+            # 对fake ground truth做二值化以备训练
+            raw_pos_fake_ground_truth = self.threshold_transform(raw_pos_fake_ground_truth)
+
+            print("loading image and ground truth from...")
+            print(self.test_images[index])
+            print(self.test_ground_truths[index])
+            print(self.test_pos_gts[index])
+            print(filenames)
+
+        # data augmentation
+        if self.data_aug:
+            raw_image, raw_ground_truth, raw_pos_fake_ground_truth = augment_transform(raw_image, raw_ground_truth, raw_pos_fake_ground_truth, target_size=self.model_size)
+
+        # 最终转化为0-1之间的数值的tensor，必须是pil image或者可以被认为是图片shape的numpy。
+        image = TF.to_tensor(raw_image)
+        ground_truth = TF.to_tensor(raw_ground_truth)
+        pos_gt = TF.to_tensor(raw_pos_fake_ground_truth)
+        filename = (os.path.basename(filenames)).split(".")[0]
+
+        return image, ground_truth, pos_gt, filename
+
+
+
+
+
+
+
+
+
+
---
+++ isic/training.py
@@ -0,0 +1,128 @@
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.transforms import functional as TF
+from torch.nn import functional as F
+import torch.optim as optim
+import os
+import configparser
+from benchmarks import *
+from visualization import *
+import numpy as np
+
+config = configparser.ConfigParser()
+config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "config.ini"))
+outlier_training_root = config["isic_train"]["outlier_root"]
+outlier_test_root = config["isic_test"]["outlier_root"]
+epochs = int(config["isic_train"]["epochs"])
+is_debug = config.getboolean("isic", "debug")
+gpu = config["isic_train"]["gpu"]
+
+
+def get_cuda_device():
+    device = torch.device("cpu:0")
+    if torch.cuda.is_available():
+        os.environ['CUDA_VISIBLE_DEVICES'] = gpu
+        device = torch.device("cuda", int(gpu))
+    return device
+
+
+def testing(test_loader, model, loss_fn, device, epoch=0):
+    target = config["isic_test"]["logdir"]
+    writer = SummaryWriter(target)
+
+    print("### Testing period of epoch {}....... ###".format(epoch))
+
+    # 此处没有训练过程
+    with torch.no_grad():
+        model.eval()
+        model.to(device)
+        iou = 0
+        index = 0
+        for idx, data in enumerate(test_loader):
+            images, ground_truths = data[0].to(device), data[1].to(device)
+            pos_gts = data[2].to(device)
+            assert images.shape[1:] == (3, 224, 224)  # format: (batch_size, frame_len, c, h, w)
+            assert ground_truths.shape[1:] == (1, 224, 224)
+            outputs = model(images)
+            loss = loss_fn(outputs, ground_truths)
+            print("The loss at epoch {} is {}...".format(epoch, loss))
+            writer.add_scalar("test/bce_loss/{}".format(epoch), loss, idx)
+            # calculate iou
+            for key, output in enumerate(outputs):
+                # get iou at each epoch
+                output_for_iou = F.sigmoid(output)
+                threshold = threshold_by_ostu(TF.to_pil_image(output_for_iou.detach().cpu())) / 255
+                temp_iou = getIOU(output_for_iou, ground_truths[key], threshold)
+                iou += temp_iou
+                index += 1
+        avg_iou = iou / index
+        print("The iou at epoch {} is {}...".format(epoch, avg_iou))
+        writer.add_scalar("test/iou", avg_iou, epoch)
+    writer.close()
+
+# TODO: save model and checkpoint
+def training(train_loader, test_loader, model, loss_fn, device):
+    """
+    训练函数，以epoch为基本单位，每一个epoch里使用fake_gt训练网络，并记录和real_gt的iou和训练loss，并进行测试
+    每一个iteration结束后，额外打印训练数据对应的模型输出作为下一轮的fake_gt。
+    :param device: gpu device
+    :param train_loader:
+    :param test_loader:
+    :param model:
+    :param loss_fn:
+    :return:
+    """
+    target = config["isic_train"]["logdir"]
+    writer = SummaryWriter(target)
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+
+    if not os.path.exists(target):
+        os.mkdir(target)
+
+    for i in range(epochs):
+        model.train()
+        model.to(device)
+        print("### the epoch {} start.... ###".format(i))
+        for idx, data in enumerate(train_loader):
+            images, ground_truths = data[0].to(device), data[1].to(device)
+            pos_gts = data[2].to(device)
+            assert images.shape[1:] == (3, 224, 224)    # format: (batch_size, frame_len, c, h, w)
+            assert pos_gts.shape[1:] == (1, 224, 224)
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = loss_fn(outputs, ground_truths)
+            loss.backward()
+            optimizer.step()
+            writer.add_scalars("train/bce_loss", {"epoch_{}".format(i): loss.item()}, idx)
+        # output result image every epoch
+        with torch.no_grad():
+            model.eval()
+            for idx, data in enumerate(train_loader):
+                index = 0
+                iou = 0
+                images, ground_truths = data[0].to(device), data[1].to(device)
+                pos_gts = data[2].to(device)
+                filename = data[3]         # might be multiple frames here, so it's two dimension array here.
+                assert images.shape[1:] == (3, 224, 224)  # format: (batch_size, frame_len, c, h, w)
+                assert ground_truths.shape[1:] == (1, 224, 224)
+                outputs = model(images)
+                for key, output in enumerate(outputs):
+                    # get iou at each epoch, using sigmoid to activate.
+                    output_for_iou = F.sigmoid(output)
+                    threshold = threshold_by_ostu(TF.to_pil_image(output_for_iou.detach().cpu()))/255
+                    print("the threshold is {}...".format(threshold))
+                    temp_iou = getIOU(output_for_iou, ground_truths[key], threshold)
+                    # record the outliers when iou is less than 0.5
+                    if is_debug and temp_iou < 0.5:
+                        output_for_vis = 255*binarify(output_for_iou, threshold)
+                        visualize_outlier(config["isic_train"]["outlier_root"], images[key].detach().cpu(), output_for_vis.detach().cpu(), ground_truths[key].detach().cpu(), pos_gts[key].detach().cpu(), i, "training", filename)
+                    iou += temp_iou
+                    index += 1
+                avg_iou = iou / index
+                print("The iou at batch {} in epoch {} is {}...".format(i, idx, avg_iou))
+                writer.add_scalars("train/iou", {"epoch_{}".format(i): avg_iou}, idx)
+        # 测试嵌套在每一个epoch训练完之后
+        testing(test_loader, model, loss_fn, device, i)
+    writer.close()
+
+
+
